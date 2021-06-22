1c9358965fa269ebb735212a88e0bbd4abff2cf7
Rodrigo Novas <rodrigo.novas@skf.com>
2021-06-22 12:55:31 -0300

    Add new nodes modules: bootwatch and fontawesome

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import logo from './logo-cafe.svg';
 import './App.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faFacebook } from "@fortawesome/free-brands-svg-icons"
+import { faHome } from "@fortawesome/free-solid-svg-icons";
 
+//para usar favicon coloque el comando npm i --save @fortawesome/fontawesome-svg-core  @fortawesome/free-solid-svg-icons @fortawesome/react-fontawesome
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
+      <div>
+      <FontAwesomeIcon icon={faHome} />
+      <FontAwesomeIcon icon={faFacebook} />
+      </div>
       </header>
     </div>
   );
